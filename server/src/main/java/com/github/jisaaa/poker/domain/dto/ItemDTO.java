efa07e0dd3930ca00ed67e07084a718c6390c2e2
Jisaaa <jisaaa@github.com>
2026-07-05 19:01:12 +0800

    fix: ItemDTO add @JsonProperty for snake_case JSON mapping

--- server/src/main/java/com/github/jisaaa/poker/domain/dto/ItemDTO.java
+++ server/src/main/java/com/github/jisaaa/poker/domain/dto/ItemDTO.java
@@ -1,5 +1,6 @@
 package com.github.jisaaa.poker.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,13 +14,19 @@ import java.util.List;
 @AllArgsConstructor
 public class ItemDTO {
     private String id;
+    @JsonProperty("display_name")
     private String displayName;
     private String description;
     private int price;
     private int rarity;
+    @JsonProperty("item_type")
     private int itemType; // 0=joker 1=consumable
+    @JsonProperty("shop_weights")
     private List<Integer> shopWeights;
+    @JsonProperty("upgrade_costs")
     private List<Integer> upgradeCosts;
+    @JsonProperty("effect_class")
     private String effectClass;
+    @JsonProperty("level_params")
     private List<Object> levelParams;
 }
